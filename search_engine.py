05b5c2031324b5cd9fd48e1d460575b0d663bbf0
Teddy Hartanto <hartantoteddy@yahoo.com>
2016-09-03 02:11:00 +0300

    Refactored the code into functions. Still ugly, but well...

--- search_engine.py
+++ search_engine.py
@@ -4,17 +4,24 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import sys
 
-url = sys.argv[1]
 
 # crawling
-resp = requests.get(url)
-while resp.status_code != 200:
+def download_page(url):
     resp = requests.get(url)
+    while resp.status_code != 200:
+        resp = requests.get(url)
+    return resp.text
 
-path = urlparse(url).path.split('/')
-uid = path[-3]
+def parse_html(url, html):
+    path = urlparse(url).path.split('/')
+    uid = path[-3]
+    soup = BeautifulSoup(html, 'html.parser')
+    selected = soup.select('div#thing_t3_{0} div.md'.format(uid))[0]
+    return selected.get_text()
 
-# parsing
-soup = BeautifulSoup(resp.text, 'html.parser')
-selected = soup.select('div#thing_t3_{0} div.md'.format(uid))[0]
-print(selected.get_text())
+if __name__ == '__main__':
+    url = sys.argv[1]
+    html = download_page(url)
+    content = parse_html(url, html)
+    print(content)
+    
